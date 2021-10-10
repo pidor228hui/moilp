d2ad83b5bd9ef5bf90b564a916e619779a3766d6
pidor228hui <78651236+pidor228hui@users.noreply.github.com>
2021-10-11 00:17:53 +0300

    Add files via upload

--- commands/info.py
+++ commands/info.py
@@ -13,7 +13,7 @@ user = Blueprint(
 )
 
 
-@user.on.message_handler(FromMe(), text=" лп")
+@user.on.message_handler(FromMe(), text="{service_prefix} лп")
 @logger_decorator
 async def info_wrapper(message: Message, **kwargs):
     db = Database.get_current()
@@ -26,31 +26,9 @@ async def info_wrapper(message: Message, **kwargs):
         update_text = ""
 
     text = f"""
-    🦊 Drocher228 by {__author__} 
-    
-    ⚠️ Удаление уведов: {"on&#9989;" if db.delete_all_notify else "off&#10060;"}
-    🔕 Вкл/Выкл уведы: {"on&#9989;" if db.disable_notifications else "off&#10060;"}
-    
-    ☯️ ИгнорЛист: {len(db.ignored_members)}
-    ♻️ Гл.ИгнорЛист: {len(db.ignored_global_members)}
-    🔇 Muted: {len(db.muted_members)}
-    ❤️ Dovs: {len(db.trusted)}
-    
-    🚶 Leave chat: {"on&#9989;" if db.auto_exit_from_chat else "off&#10060;"}
-    🔞 Deleted chat: {"on&#9989;" if db.auto_exit_from_chat_delete_chat else "off&#10060;"}
-    🏳️‍🌈 Вчс добавляющих: {"on&#9989;" if db.auto_exit_from_chat_add_to_black_list else "off&#10060;"}
-    
-    🗨️ Повторялка: {"on&#9989;" if db.repeater_active else "off&#10060;"}
-    💫 Префикс повторялки: {db.repeater_word}
-    
-    🤗 Ответка в ебало: {"on&#9989;" if db.bio_reply else "off&#10060;"}
-        
-    ⚜️Удалялка: {db.dd_prefix}
-    🔱Префиксы ЛП: {' '.join(db.service_prefixes)}
-    ⚕️Мои префиксы: {' '.join(db.self_prefixes) if db.self_prefixes else ''}
-    🔺Повторялка IDM: {' '.join(db.duty_prefixes) if db.duty_prefixes else ''}
+    🦊 Drocher228 \n by{__author__} Version:({__version__})
     [https://vk.com/wall-206192128_5|Команды этого LP тут]
-    V{__version__} Updates:{update_text}
+    🔎Updates:{update_text}
   
     """.replace('    ', '')
     await edit_message(
