05b671836deac7be8d0884b70df3ca2977fef70e
pidor228hui <78651236+pidor228hui@users.noreply.github.com>
2021-10-12 16:41:44 +0300

    Add files via upload

--- commands/info.py
+++ commands/info.py
@@ -36,7 +36,40 @@ async def info_wrapper(message: Message, **kwargs):
     
     🛡️Алиасы: {len(db.aliases)}
     
+   [https://vk.com/wall-206192128_5|commands]
+    
     {update_text}
+    """.replace('    ', '')
+    await edit_message(
+        message,
+        text
+    )
+
+
+@user.on.message_handler(FromMe(), text="<prefix:service_prefix> чаты")
+@logger_decorator
+async def info_wrapper(message: Message, **kwargs):
+    db = Database.get_current()
+    version_rest = requests.get(const.VERSION_REST).json()
+
+    if version_rest['version'] != const.__version__:
+        update_text = f"\n\n Обновление у вас\n на глазах📡"  \
+                      f"\n"
+    else:
+        update_text = ""
+
+    text = f"""
+    🦊 Drocher228 by {__author__}\n V{__version__}
+    
+    ⚙️Команды связанные с чатами:
+    
+    🔕Удаление уведомлений:{"on&#9989;" if db.delete_all_notify else "off&#10060;"}
+    🔔Вкл/Выкл уведомлений:{"on&#9989;" if db.disable_notifications else "off&#10060;"}
+    🚶Автовыход с бесед:{"on&#9989;" if db.auto_exit_from_chat else "off&#10060;"}
+    🏃Автовыход\n и удаление:{"on&#9989;" if db.auto_exit_from_chat_delete_chat else "off&#10060;"}
+    📃Добавлять\n пригласившего в ЧС:{"on&#9989;" if db.auto_exit_from_chat_add_to_black_list else "off&#10060;"}
+    🤗 Ответка в ебало:{"on&#9989;" if db.bio_reply else "off&#10060;"}
+    
     """.replace('    ', '')
     await edit_message(
         message,
